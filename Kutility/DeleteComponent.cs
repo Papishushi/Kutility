d723f46606f09f44cdc36a03d7be4bb7dc8ccfc1
Daniel Molinero Lucas <danielmolinero111@gmail.com>
2020-11-19 05:30:33 +0100

    First additions: Singleton & DeleteComponent
    Added 2 tools for developers.
    
    -Singleton creates an easy to implement structure for the editor and standalone versions.
    -DeleteComponent destroys inmediately any given component.

---
+++ Kutility/DeleteComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Kutility
+{
+    /// <summary>
+    /// Destroys inmediatelly any component.
+    /// </summary>
+    public class DeleteComponent : Object
+    {
+        /// <summary>
+        /// Destroys inmediatelly a given component.
+        /// </summary>
+        /// <param name="componentReference">The component to be destroyed.</param>
+        public DeleteComponent(Component componentReference)
+        {
+            DestroyImmediate(componentReference);
+            DestroyImmediate(this);
+        }
+    }
+}
